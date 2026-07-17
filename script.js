7aff6871b55307dc24b76f6d6d125e062afbc167
aapirant2003-afk <aapirant2003@gmail.com>
2026-07-17 14:30:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+    });
+});
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".skill-card,.project-card,.about-container,.contact-box").forEach(el=>{
+    el.classList.add("hidden");
+    observer.observe(el);
+});
